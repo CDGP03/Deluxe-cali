a9ef6f5b43f0100e5393b791f89ed62ddd50370c
Cristian David González Pillimue <cgonsalezpillimue@gmail.com>
2026-07-28 22:32:05 -0500

    home tsx

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Leemos las credenciales del archivo .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
-
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 // Creamos y exportamos el cliente para usarlo en toda la app
 export const supabase = createClient(supabaseUrl, supabaseKey)
